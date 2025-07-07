17402cf2d53c03755ba916c9609daf464360c4e3
Nishant Hattarki <nishant.231ee139@nitk.edu.in>
2025-07-07 17:06:41 +0530

    Add files via upload

---
+++ restaurant/views.py
@@ -0,0 +1,60 @@
+from django.shortcuts import render
+from django.views import View
+from django.contrib.auth.mixins import UserPassesTestMixin, LoginRequiredMixin
+from django.utils.timezone import datetime
+from customer.models import OrderModel
+
+
+class Dashboard(LoginRequiredMixin,View):
+    def get(self, request, *args, **kwargs):
+        # get the current date
+        username = "None"
+        if request.user.is_authenticated:
+            username = request.user.username
+        today = datetime.today()
+        orders = OrderModel.objects.filter(restaurant__uname = username)
+
+        # loop through the orders and add the price value, check if order is not shipped
+        unshipped_orders = []
+        total_revenue = 0
+        for order in orders:
+            total_revenue += order.price
+            if not order.is_delivered:
+                unshipped_orders.append(order)
+
+        # pass total number of orders and total revenue into template
+        context = {
+            'orders': unshipped_orders,
+            'total_revenue': total_revenue,
+            'total_orders': len(orders)
+        }
+
+        return render(request, 'restaurant/dashboard.html', context)
+
+    def test_func(self):
+        return self.request.user.groups.filter(name='Staff').exists()
+
+
+class OrderDetails(LoginRequiredMixin, View):
+    def get(self, request, pk, *args, **kwargs):
+        order = OrderModel.objects.get(pk=pk)
+        context = {
+            'order': order,
+            'items'  : order.items
+        }
+
+        return render(request, 'restaurant/order-details.html', context)
+
+    def post(self, request, pk, *args, **kwargs):
+        order = OrderModel.objects.get(pk=pk)
+        order.is_shipped = True
+        order.save()
+
+        context = {
+            'order': order
+        }
+
+        return render(request, 'restaurant/order-details.html', context)
+
+    def test_func(self):
+        return self.request.user.groups.filter(name='Staff').exists()
